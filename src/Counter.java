f58561e0ad6df1ec35481c963c9542b8c0207e37
Crisis82 <filmerlo99@gmail.com>
2020-01-15 16:54:59 +0100

    service and counter created

---
+++ src/Counter.java
@@ -0,0 +1,11 @@
+public class Counter {
+
+    private Service serviceType;
+
+
+    public Counter(Service service) {
+        serviceType = service;
+    }
+
+
+}
---
+++ src/Service.java
@@ -0,0 +1,5 @@
+public enum Service {
+    PRELIEVO,
+    DEPOSITO,
+    ALTRO;
+}
